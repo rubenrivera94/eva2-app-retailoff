5abb40a20fc6cda8a7bafa4d15d5528deac4f523
rubenrivera94 <sdigitalx.adm@gmail.com>
2024-08-29 10:20:18 -0400

    Se subio proyecto Ionic Evaluacion 2 App RetailOff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyCsPLS9v2kpHripzOFONSoFy9PYTcCzrMs",
+    authDomain: "login-2-bcdfa.firebaseapp.com",
+    projectId: "login-2-bcdfa",
+    storageBucket: "login-2-bcdfa.appspot.com",
+    messagingSenderId: "203086653429",
+    appId: "1:203086653429:web:43c4581d33f0249c8322bc"
+  }
 };
 
 /*
